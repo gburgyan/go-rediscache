708e43b0e559d1eff323eda06502bfd1283ab1f0
George Burgyan <gburgyan@vec.com>
2024-08-12 10:55:44 -0500

    Added benchmarks.

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,6 +2,7 @@ package rediscache
 
 import (
 	"context"
+	"github.com/gburgyan/go-timing"
 	"github.com/go-redis/redismock/v8"
 	"testing"
 )
@@ -42,5 +43,29 @@ func Benchmark_CacheHit(b *testing.B) {
 		mock.ClearExpect()
 		mock.ExpectGet(key).SetVal(val)
 		_ = cfn("test-key")
+	}
+}
+
+func Benchmark_CacheHit_Timing(b *testing.B) {
+	fn := func(_ context.Context, s string) string {
+		return s
+	}
+
+	opts := CacheOptions{}
+	ctx := context.Background()
+	mockRedis, mock := redismock.NewClientMock()
+
+	cache := NewRedisCache(ctx, mockRedis, opts)
+
+	cfn := Cache(cache, fn)
+	key := "GoCache-2631e43346c3d8b4a341e560ec2b609b6db0250448ce49a048f4197e8558cc3b"
+	serVal, _ := serialize([][]byte{[]byte("value")})
+	val := string(serVal)
+
+	for i := 0; i < b.N; i++ {
+		timingCtx := timing.Root(ctx)
+		mock.ClearExpect()
+		mock.ExpectGet(key).SetVal(val)
+		_ = cfn(timingCtx, "test-key")
 	}
 }
